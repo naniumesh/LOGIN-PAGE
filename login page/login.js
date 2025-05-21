535b0924d91aefb154df5cac7a5264e28a3146e4
MALLEMKONDU UMESH <75903669+naniumesh@users.noreply.github.com>
2025-05-21 16:26:15 +0530

    Update login.js

--- login page/login.js
+++ login page/login.js
@@ -21,7 +21,7 @@ async function login() {
   }
 
   try {
-    const res = await fetch("http://localhost:3000/api/login", {
+    const res = await fetch("https://login-page-6jzv.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password, adminType })
